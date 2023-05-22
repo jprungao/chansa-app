bc0e2ed0def9fe72cafb228a954484b7f1c2be55
John Parungao <nhojparungao@gmail.com>
2023-05-22 11:36:47 +0800

    added current live match

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatchComponent } from './match/match.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MatchComponent
   ],
   imports: [
     BrowserModule,
